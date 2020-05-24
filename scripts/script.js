4e182909362c26eac1d2a311f726a0225bcd5b8c
Ciaran Concannon <cjcon90@protonmail.com>
2020-05-24 17:53:16 +0100

    added functioning score counters

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,120 @@
+let userScore = 0;
+let computerScore = 0;
+const userCounter = document.getElementById("user-score");
+const computerCounter = document.getElementById("computer-score");
+const winner = document.getElementById("result");
+const playerRock = document.getElementById("lrock");
+const playerPaper = document.getElementById("lpaper");
+const playerScissors = document.getElementById("lscissors");
+
+
+
+
+// Automated Computer Choice
+
+function computerPlay() {
+    const choices = ["rock", "paper", "scissors"]
+    return choices[Math.floor(Math.random() * Math.floor(choices.length))]
+}
+
+
+function win() {
+    console.log("Yay! You win!!");
+    userScore++
+    userCounter.innerHTML = userScore;
+}
+
+function draw() {
+    console.log("it's a draw!");
+}
+
+function lose() {
+    console.log("Sorry, you lose....");
+    computerScore++
+    computerCounter.innerHTML = computerScore;
+
+}
+
+
+
+function game(playerWeapon) {
+const computerWeapon = computerPlay();
+console.log("Computer chooses: " + computerWeapon);
+console.log("You choose: " + playerWeapon)
+
+if ((playerWeapon == 'rock' && computerWeapon === 'scissors') || (playerWeapon == 'paper' && computerWeapon === 'rock') || (playerWeapon == 'scissors' && computerWeapon === 'paper')) {
+    win();
+}
+
+else if ((playerWeapon == 'rock' && computerWeapon === 'paper') || (playerWeapon == 'paper' && computerWeapon === 'scissors') || (playerWeapon == 'scissors' && computerWeapon === 'rock')) {
+    lose();
+}
+
+else {
+    draw();
+}
+
+}
+
+
+function main() {
+    playerRock.addEventListener('click', function () {
+        game('rock');
+    })
+
+    playerPaper.addEventListener('click', function () {
+        game('paper');
+    })
+
+    playerScissors.addEventListener('click', function () {
+        game('scissors');
+    })
+    
+}
+
+
+
+
+
+
+
+main();
+
+
+
+
+
+
+
+
+
+
+
+
+/*const playerRock = document.querySelector("lrock");
+const playerPaper = document.querySelector("lpaper");
+const playerScissors = document.querySelector("lscissors");
+const playerSelect = document.getElementsByClassName("l-img-div");
+
+
+
+
+playerRock.onclick = function rockSelect(playerWeapon) {
+    return 'rock';
+    console.log(playerWeapon)
+}
+
+playerPaper.onclick = function paperSelect(playerWeapon) {
+    return 'paper';
+}
+
+playerScissors.onclick = function scissorsSelect(playerWeapon) {
+    return 'scissors';
+}
+
+console.log(playerWeapon);
+
+
 
 // Automated Computer Choice
 const choices = ["rock", "paper", "scissors"]
@@ -12,7 +129,7 @@ const computerSelection = computerPlay();
 
 
 // Single Round
-function playRound(playerSelection, computerSelection) {
+/*function playRound(playerSelection, computerSelection) {
 
     if ((playerSelection != 'rock') && (playerSelection != 'paper') && (playerSelection != 'scissors')) {
         return playerSelection + " isn't in the rules!! DISQUALIFIED"
@@ -37,4 +154,4 @@ playRound();
 console.log("Player chooses: " + playerSelection);
 console.log("Computer chooses: " + computerSelection);
 alert("Computer chooses " + computerSelection + "...")
-alert(playRound(playerSelection, computerSelection));
+alert(playRound(playerSelection, computerSelection));*/
