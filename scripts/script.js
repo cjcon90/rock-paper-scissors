7b7efd82fc725beb1b0a65033868c8120178d8a0
Ciaran Concannon <cjcon90@protonmail.com>
2020-05-24 19:22:09 +0100

    added computer arm movement / final score update / minor responsiveness

--- scripts/script.js
+++ scripts/script.js
@@ -2,156 +2,128 @@ let userScore = 0;
 let computerScore = 0;
 const userCounter = document.getElementById("user-score");
 const computerCounter = document.getElementById("computer-score");
-const winner = document.getElementById("result");
 const playerRock = document.getElementById("lrock");
 const playerPaper = document.getElementById("lpaper");
 const playerScissors = document.getElementById("lscissors");
+const playerRockImg = document.getElementById("left-rock");
+const playerPaperImg = document.getElementById("left-paper");
+const playerScissorsImg = document.getElementById("left-scissors");
+const compRockImg = document.getElementById("right-rock");
+const compPaperImg = document.getElementById("right-paper");
+const compScissorsImg = document.getElementById("right-scissors");
+const scoreboard = document.getElementById("result");
+let playerChoice = play();
+let computerChoice = '';
+let winner = '';
 
 
 
 
-// Automated Computer Choice
-
-function computerPlay() {
-    const choices = ["rock", "paper", "scissors"]
-    return choices[Math.floor(Math.random() * Math.floor(choices.length))]
-}
-
-
-function win() {
-    console.log("Yay! You win!!");
-    userScore++
-    userCounter.innerHTML = userScore;
-}
-
-function draw() {
-    console.log("it's a draw!");
-}
-
-function lose() {
-    console.log("Sorry, you lose....");
-    computerScore++
-    computerCounter.innerHTML = computerScore;
-
-}
-
-
-
-function game(playerWeapon) {
-const computerWeapon = computerPlay();
-console.log("Computer chooses: " + computerWeapon);
-console.log("You choose: " + playerWeapon)
-
-if ((playerWeapon == 'rock' && computerWeapon === 'scissors') || (playerWeapon == 'paper' && computerWeapon === 'rock') || (playerWeapon == 'scissors' && computerWeapon === 'paper')) {
-    win();
-}
-
-else if ((playerWeapon == 'rock' && computerWeapon === 'paper') || (playerWeapon == 'paper' && computerWeapon === 'scissors') || (playerWeapon == 'scissors' && computerWeapon === 'rock')) {
-    lose();
-}
-
-else {
-    draw();
-}
-
-}
-
-
-function main() {
+// Player Selection-----------------------
+function play() {
     playerRock.addEventListener('click', function () {
         game('rock');
+        playerChoice = "Rock";
+        playerRockImg.style.right = "0px"
+        playerPaperImg.style.right = "50%"
+        playerScissorsImg.style.right = "50%"
     })
 
     playerPaper.addEventListener('click', function () {
         game('paper');
+        playerChoice = "Paper";
+        playerRockImg.style.right = "50%"
+        playerPaperImg.style.right = "0px"
+        playerScissorsImg.style.right = "50%"
     })
 
     playerScissors.addEventListener('click', function () {
         game('scissors');
+        playerChoice = "Scissors";
+        playerRockImg.style.right = "50%"
+        playerPaperImg.style.right = "50%"
+        playerScissorsImg.style.right = "0px"
     })
-    
+
 }
 
+play();
 
 
 
 
+// Computer Selection---------------------
 
-
-main();
-
-
-
-
-
-
-
-
-
-
-
-
-/*const playerRock = document.querySelector("lrock");
-const playerPaper = document.querySelector("lpaper");
-const playerScissors = document.querySelector("lscissors");
-const playerSelect = document.getElementsByClassName("l-img-div");
-
-
-
-
-playerRock.onclick = function rockSelect(playerWeapon) {
-    return 'rock';
-    console.log(playerWeapon)
-}
-
-playerPaper.onclick = function paperSelect(playerWeapon) {
-    return 'paper';
-}
-
-playerScissors.onclick = function scissorsSelect(playerWeapon) {
-    return 'scissors';
-}
-
-console.log(playerWeapon);
-
-
-
-// Automated Computer Choice
-const choices = ["rock", "paper", "scissors"]
 function computerPlay() {
-    return choices[Math.floor(Math.random() * Math.floor(choices.length))]
+    const choices = ["rock", "paper", "scissors"]
+    computerSelect = choices[Math.floor(Math.random() * Math.floor(choices.length))]
+
+    if (computerSelect === 'rock') {
+        compRockImg.style.left = "0px"
+        compPaperImg.style.left = "200px"
+        compScissorsImg.style.left = "200px"
+        computerChoice = computerSelect;
+    }
+
+    if (computerSelect === 'paper') {
+        compRockImg.style.left = "200px"
+        compPaperImg.style.left = "0px"
+        compScissorsImg.style.left = "200px"
+        computerChoice = computerSelect;
+    }
+
+    if (computerSelect === 'scissors') {
+        compRockImg.style.left = "200px"
+        compPaperImg.style.left = "200px"
+        compScissorsImg.style.left = "0px"
+        computerChoice = computerSelect;
+    }
+
+    return computerSelect;
 }
 
 
-//Testing scissors vs random selection
-//const playerSelection = prompt("Rock, Paper or Scissors?", "").toLowerCase();
-const computerSelection = computerPlay();
 
 
-// Single Round
-/*function playRound(playerSelection, computerSelection) {
 
-    if ((playerSelection != 'rock') && (playerSelection != 'paper') && (playerSelection != 'scissors')) {
-        return playerSelection + " isn't in the rules!! DISQUALIFIED"
+// Determining who wins ----------------
+
+function game(playerWeapon) {
+    const computerWeapon = computerPlay();
+    console.log("Computer chooses: " + computerWeapon);
+    console.log("You choose: " + playerWeapon)
+
+    if ((playerWeapon == 'rock' && computerWeapon === 'scissors') || (playerWeapon == 'paper' && computerWeapon === 'rock') || (playerWeapon == 'scissors' && computerWeapon === 'paper')) {
+        win();
     }
 
-    else if ((playerSelection == 'rock' && computerSelection === 'scissors') || (playerSelection == 'paper' && computerSelection === 'rock') || (playerSelection == 'scissors' && computerSelection === 'paper')) {
-        return "Yay! You win!!";
-    }
-
-    else if ((playerSelection == 'rock' && computerSelection === 'paper') || (playerSelection == 'paper' && computerSelection === 'scissors') || (playerSelection == 'scissors' && computerSelection === 'rock')) {
-        return "Sorry, you lose....";
+    else if ((playerWeapon == 'rock' && computerWeapon === 'paper') || (playerWeapon == 'paper' && computerWeapon === 'scissors') || (playerWeapon == 'scissors' && computerWeapon === 'rock')) {
+        lose();
     }
 
     else {
-        return "it's a draw!";
+        draw();
     }
 }
 
-playRound();
 
-// Display player / computer / result in console
-console.log("Player chooses: " + playerSelection);
-console.log("Computer chooses: " + computerSelection);
-alert("Computer chooses " + computerSelection + "...")
-alert(playRound(playerSelection, computerSelection));*/
+// Updating Scoreboard with winner --------------------
+
+function win() {
+    winner = "Yay! You win!!";
+    userScore++
+    userCounter.innerHTML = userScore;
+    scoreboard.innerHTML = playerChoice + " vs " + computerChoice + ": " + winner;
+}
+
+function draw() {
+    winner = "it's a draw!";
+    scoreboard.innerHTML = playerChoice + " vs " + computerChoice + ": " + winner;
+}
+
+function lose() {
+    winner = "Sorry, you lose....";
+    computerScore++
+    computerCounter.innerHTML = computerScore;
+    scoreboard.innerHTML = playerChoice + " vs " + computerChoice + ": " + winner;
+}
